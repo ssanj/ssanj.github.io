c197f61c69269364d867ce804344c8f8395bdc47
sanjiv sahayam <sanjsmailbox@gmail.com>
2014-07-15 21:18:14 +1000

    added search capabilities.

---
+++ js/babyloncandle.js
@@ -0,0 +1,22 @@
+var pages = new Bloodhound({
+  datumTokenizer: function(datum) {
+       var tokens = datum.title.split(" ");
+       return tokens.concat($.map(datum.tags.split(","), function(e,i) { return e.trim() }));
+  },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: '/data/pages.json'
+});
+ 
+pages.initialize(); 
+ 
+$('#remote .typeahead').typeahead(null, {
+  name: 'blog-titles',
+  displayKey: 'title',
+  source: pages.ttAdapter(),
+  hint: false,
+  templates:{
+    suggestion: Handlebars.compile('<p class="bcandle.suggestion"><a href="{{url}}"><strong>{{title}}</strong></a></p>')
+  }
+}).bind("typeahead:selected", function(obj, datum, name) {
+  window.location=datum.url;
+});
